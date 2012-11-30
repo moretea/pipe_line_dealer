e0cdc3fd8c6a57e6fad770ffe15e2b76970673ef
Maarten Hoogendoorn <maarten@moretea.nl>
2012-11-30 22:30:55 +0100

    Decrease complexity of ResultFetcher implementation

--- lib/pipe_line_dealer/collection/concerns/results_fetcher.rb
+++ lib/pipe_line_dealer/collection/concerns/results_fetcher.rb
@@ -4,58 +4,66 @@ module PipeLineDealer
       class ResultsFetcher
         attr_reader :options
 
+        # This struct captures the state we need to implement an transparent API
+        Cursor = Struct.new(:params, :results_yielded, :done)
+
         def initialize(collection, options)
           @collection = collection
-          @options = options
-        end
-
-        def limit
-          options[:limit] || PipeLineDealer::Limits::MAX_RESULTS_PER_PAGE
+          @options    = options
         end
 
         def each &block
-          @params =  { page: 1, per_page: PipeLineDealer::Limits::MAX_RESULTS_PER_PAGE }
+          cursor = Cursor.new(build_params, 0, false)
 
-          @results_yielded = 0
+          while not cursor.done
+            status, result = @collection.client.connection.get(@collection.collection_url + ".json", cursor.params)
 
-          # Enforce maximum number of resulst per page.
-          if limit && limit > PipeLineDealer::Limits::MAX_RESULTS_PER_PAGE
-            options[:limit] = PipeLineDealer::Limits::MAX_RESULTS_PER_PAGE
-          end
-
-          @params[:per_page] = limit
-
-          #TODO: refactor this deal specific piece of code.
-          if options.has_key?(:where) && options[:where].has_key?(:query) && options[:where][:query].has_key?(:company_id)
-            @params[:company_id] = options[:where][:query][:company_id].to_s
-          end
-
-          catch :stop_yielding do
-            while true
-              status, result = @collection.client.connection.get(@collection.collection_url + ".json", @params)
-
-              if status == 200
-                yield_results(result, &block)
-              else # status != 200
-                #TODO
-                raise "Unexpected status #{status.inspect}"
-              end
+            if status == 200
+              yield_results(cursor, result, &block)
+            else
+              raise "Unexpected status #{status.inspect}" #TODO: Nicer exception
             end
           end
         end
 
-        def yield_results result, &block
-          result["entries"].each do |entry|
-            block.call(@collection.klass.new(collection: @collection, persisted: true, attributes: entry))
-            @results_yielded += 1
+        protected
 
-            throw :stop_yielding if @results_yielded >= limit
+        def build_params
+          params = { page: 1, per_page: PipeLineDealer::Limits::MAX_RESULTS_PER_PAGE }
+
+          # Enforce maximum number of resulst if limit is smaller than the max results per page.
+          if options[:limit] && options[:limit] < PipeLineDealer::Limits::MAX_RESULTS_PER_PAGE
+            params[:per_page] = options[:limit]
           end
 
+          # Additional condition.
+          #TODO: refactor?
+          if options.has_key?(:where) && options[:where].has_key?(:query) && options[:where][:query].has_key?(:company_id)
+            params[:company_id] = options[:where][:query][:company_id].to_s
+          end
+
+          params
+        end
+
+        def yield_results cursor, result, &block
+          result["entries"].each do |entry|
+            block.call(@collection.klass.new(collection: @collection, persisted: true, attributes: entry))
+            cursor.results_yielded += 1
+
+            # Have we reached the limit?
+            if options[:limit] && cursor.results_yielded >= options[:limit]
+              cursor.done = true
+              return
+            end
+          end
+
+          # Are we on the last page?
           if result["pagination"]["page"] < result["pagination"]["pages"]
-            @params[:page] += 1
+            # No, go to next page
+            cursor.params[:page] += 1
           else
-            throw :stop_yielding
+            # Yes, we're done
+            cursor.done = true
           end
         end
       end
