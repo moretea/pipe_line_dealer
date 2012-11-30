071e7e59e124098e97450d19df9936ff7672b92c
Maarten Hoogendoorn <maarten@moretea.nl>
2012-11-30 22:55:31 +0100

    Refactored connection to less complex methods

--- lib/pipe_line_dealer/client/connection.rb
+++ lib/pipe_line_dealer/client/connection.rb
@@ -31,35 +31,43 @@ module PipeLineDealer
       end
 
       # HTTP methods
-      [:get, :post, :put, :delete].each do |method|
+      {get: :get_style, post: :post_style, put: :post_style, delete: :get_style}.each do |name, style|
         class_eval <<-RUBY
-          def #{method} url, params = {}
-            request(:#{method}, url, params)
+          def #{name} url, params = {}
+            request_#{style}(:#{name}, url, params)
           end
         RUBY
       end
 
       protected
 
-      def request(method, request_url, params)
-        begin
-          case method
-          when :get, :delete
-            request_params = params.merge(api_key: @key)
-            query = Faraday::Utils.build_nested_query(request_params)
-            response = @connection.send(method, request_url + "?" + query)
-          when :post, :put
-            request_url += "?api_key=#{@key}"
-            body = Faraday::Utils.build_nested_query(params)
-            response = @connection.send(method, request_url, body)
-          end
 
-          if method == :delete
-            # Bwelch. Only a delete doesn't return valid json :(
-            return [response.status, JSON.parse('{ "msg": "' + response.body + '" }')]
-          else
-            return [response.status, JSON.parse(response.body)]
-          end
+      # get / delete
+      def request_get_style method, request_url, params
+        request_params = params.merge(api_key: @key)
+        query = Faraday::Utils.build_nested_query(request_params)
+        server_response = @connection.send(method, request_url + "?" + query)
+
+        # Bwelch. Only a delete doesn't return valid json :(
+        raw = (method == :delete)
+
+        build_response_from server_response, raw: raw
+      end
+
+      # post / put
+      def request_post_style method, request_url, params
+        request_url += "?api_key=#{@key}"
+        request_body = Faraday::Utils.build_nested_query(params)
+        build_response_from @connection.send(method, request_url, request_body)
+      end
+
+      DEFAULT_REQUEST_OPTIONS = { raw: false }
+
+      def build_response_from response, options = DEFAULT_REQUEST_OPTIONS
+        if options[:raw]
+          return [response.status, JSON.parse('{ "msg": "' + response.body + '" }')]
+        else
+          return [response.status, JSON.parse(response.body)]
         end
       end
 
